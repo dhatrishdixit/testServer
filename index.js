8a77385c95c69549f08810f867f133d2f1be8b88
Dhatish Singh Dixit <dhatrish29@gmail.com>
2026-07-13 01:23:33 +0530

    test server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const app = express();
+
+app.get("/status",(req,res)=>{
+    return res.status(200).json({
+	message : `Server working as expected ${Math.floor(Math.random()*10)}`,
+	status: "pass"
+    })
+})
+
+app.listen('4000',()=>{
+    console.log("test server running on PORT: 4000")
+})
